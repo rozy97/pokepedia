30cd3a54f97ad2a33ca6da895c492988a0a04e9d
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2019-10-21 07:54:25 +0700

    connect to mongodb

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// connect to mongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongodb connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 6969;
+
+app.listen(port, () => console.log(`server listening on port ${port}`));
